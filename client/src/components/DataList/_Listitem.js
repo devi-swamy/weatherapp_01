5620a37685088d939fbdec82d9f9f64e32669fc6
deviswamy1987 <deviswamy1987@gmail.com>
2021-05-13 07:47:48 +0100

    Checking the current date

--- client/src/components/DataList/_Listitem.js
+++ client/src/components/DataList/_Listitem.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { Daysago } from "../Constants/Dates";
 
 const _Listitem = ({ CurDate }) => {
+  const today = new Date().getDate();
   return (
     <div>
       <a
         href="#"
         className={
           "list-group-item list-group-item-action " +
-          (CurDate === new Date().getDate() ? "active" : "")
+          (CurDate === today ? "active" : "")
         }
       >
         <div className="d-flex w-100 justify-content-between">
           <h5 className="mb-1">May {CurDate}</h5>
-          <small>3 days ago</small>
+          <small>{Daysago(CurDate - today)}</small>
         </div>
         <p className="mb-1"></p>
         <small>And some small print.</small>
