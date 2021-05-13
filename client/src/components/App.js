3eb8dedbea3b21a9697147ff8080978e19e3fc6b
deviswamy1987 <deviswamy1987@gmail.com>
2021-05-13 23:12:39 +0100

    Created a form for onchange and onclick

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,18 @@ import { WeatherAppContents } from "./Constants/Dates";
 
 class App extends Component {
   state = {
-    SelectedDay: 3
+    SelectedDay: -1
+  };
+  handleSelectedDayChange = e => {
+    this.setState({
+      SelectedDay: e.target.value
+    });
+  };
+
+  handleSelectedDayClick = e => {
+    this.setState({
+      SelectedDay: e.target.value
+    });
   };
   render() {
     return (
@@ -20,6 +31,29 @@ class App extends Component {
                 WeatherAppContents={WeatherAppContents}
                 SelectedDay={this.state.SelectedDay}
               ></List>
+            </div>
+            <div className="col-8">
+              <form action="">
+                <div className="form-group">
+                  <label htmlFor="dateSelected">Select a Day</label>
+                  <select
+                    id="dateSelected"
+                    className="form-control"
+                    value={this.state.SelectedDay}
+                    onChange={this.handleSelectedDayChange}
+                    onClick={this.handleSelectedDayClick()}
+                  >
+                    <option value={null} disabled>
+                      Please Select ...{" "}
+                    </option>
+                    {WeatherAppContents.map((date, key) => (
+                      <option value={key} key={key}>
+                        May {date.Date}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </form>
             </div>
           </div>
         </div>
