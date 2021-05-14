c4822ec355bc81f7b0487845779a4f5aec4e2562
deviswamy1987 <deviswamy1987@gmail.com>
2021-05-14 01:06:08 +0100

    Creating and passing the click event handler

--- client/src/components/DataList/List.js
+++ client/src/components/DataList/List.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Listitem from "./_Listitem";
 
-const List = ({ WeatherAppContents, SelectedDay }) => {
+const List = ({ WeatherAppContents, SelectedDay, handleSelectedDayClick }) => {
   return (
     <div className="List list-group">
       {WeatherAppContents.map((cur, key) => (
         <Listitem
           key={key}
+          index={key}
           CurDate={cur.Date}
           selected={+SelectedDay === key}
+          handleSelectedDayClick={handleSelectedDayClick}
         ></Listitem>
       ))}
     </div>
